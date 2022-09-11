f1b55fed41ce669aa1b2578de7ac895bbc482235
day_mac <m.dayyan367@gmail.com>
2022-09-11 12:20:21 -0600

    somewhat changes

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -4,44 +4,43 @@ import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { useAuth } from "../Contexts/AuthContext";
 
 export default function Login() {
-    const { login } = useAuth();
+  const { login } = useAuth();
 
-    return (
-        <div className="login-container">
-            <div className="login-box">
-                <div className="login-box-top">
-                    <img
-                        src={require("../Assets/images/logo.png")}
-                        alt="friendr logo"
-                        id="login-logo-img"
-                    />
-                    <h1 id="title-logo">Friender</h1>
-                </div>
-                <div className="login-box-bottom">
-                    <div className="google-login-container">
-                        <GoogleOAuthProvider
-                            clientId={process.env.REACT_APP_OAUTH_CID}
-                        >
-                            <GoogleLogin
-                                onSuccess={(credentialResponse) => {
-                                    login(credentialResponse);
-                                }}
-                                onError={() => {
-                                    console.log("Login Failed");
-                                }}
-                            />
-                        </GoogleOAuthProvider>
-                    </div>
-                    <div className="login-box-links">
-                        <a
-                            href="https://github.com/LIQUIDmonkeyyy/Friender"
-                            className="login-box-link"
-                        >
-                            Github
-                        </a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <div className="login-box-top">
+          <img
+            src={require("../Assets/images/logo.png")}
+            alt="friendr logo"
+            id="login-logo-img"
+          />
+          <h1 id="title-logo">Friender</h1>
+          <div>AN APP FOR FRIENDS</div>
         </div>
-    );
+        <div className="login-box-bottom">
+          <div className="google-login-container">
+            <GoogleOAuthProvider clientId={process.env.REACT_APP_OAUTH_CID}>
+              <GoogleLogin
+                onSuccess={(credentialResponse) => {
+                  login(credentialResponse);
+                }}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+              />
+            </GoogleOAuthProvider>
+          </div>
+          <div className="login-box-links">
+            <a
+              href="https://github.com/LIQUIDmonkeyyy/Friender"
+              className="login-box-link"
+            >
+              Github
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
